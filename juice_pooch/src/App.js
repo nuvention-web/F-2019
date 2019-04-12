bdfe7228d449e5c25bf930c15d422cd7c2de2708
Sam Arrants <arrants8@gmail.com>
2019-04-12 16:45:16 -0500

    Checkout modules change color based on interactions

--- juice_pooch/src/App.js
+++ juice_pooch/src/App.js
@@ -11,6 +11,8 @@ class App extends Component {
     return (
       <div className="App">
         <CaseComponent/>
+        <CaseComponent/>
+        <CaseComponent/>
 
       </div>
     );
--- juice_pooch/src/components/Case/Case.js
+++ juice_pooch/src/components/Case/Case.js
@@ -2,30 +2,43 @@ import React, { Component } from 'react';
 // import Card from '@material-ui/core/Card';
 import './style.css';
 
-const styles = theme => ({
-  card: {
-    maxWidth: 345,
-    height: 100,
-  },
-});
-
 
 class CaseComponent extends Component {
+  constructor(){
+      super();
+
+      this.state = {
+         toggled: true
+      }
+  }
+
+  changeColor(){
+    this.setState({toggled: !this.state.toggled})
+  }
+
+  // const styles = theme => ({
+  //   card: {
+  //     maxWidth: 345,
+  //     height: 100,
+  //   },
+  // });
 
   render() {
+    let box_class = this.state.toggled ? "greenBox" : "redBox";
+
+
     var datetime = new Date();
     var data = this.props.data;
     return (
       <div >
         <div style={{textAlgin:"center"}}>
-        	<div style={{backgroundColor:"green",width:"80%",height:"100px",display:"inline-block",
-        				borderRadius:"10px"}} >  		
+        	<div className={box_class} >
         		<form style={{display:"flex"}}>
         		<div>
         			<p> ID </p>
-        			<p> 10 </p>  
-        		</div> 
-        		<div>   
+        			<p> 10 </p>
+        		</div>
+        		<div>
         			<label>
         				Renter: <br />
     					<input type="text" name="name" />
@@ -34,7 +47,7 @@ class CaseComponent extends Component {
   				<div>
         			<label>
         				Returned: <br />
-    					<input type="checkbox" name="name" />
+    					<input type="checkbox" onClick={this.changeColor.bind(this)} name="name" />
   					</label>
   				</div>
         		</form>
