13461d121efb725c69f3467c4c9046ce6621930b
Sam Arrants <arrants8@gmail.com>
2019-04-17 19:14:45 -0500

    added time stamps, buttons, and state changes

--- juice_pooch/src/components/Case/Case.js
+++ juice_pooch/src/components/Case/Case.js
@@ -8,13 +8,36 @@ class CaseComponent extends Component {
       super();
 
       this.state = {
-         toggled: true
+         checked_out: false,
+         checkOutTime: new Date(),
+         value: ""
+
       }
+      this.handleChange = this.handleChange.bind(this);
+      this.handleSubmit = this.handleSubmit.bind(this);
   }
 
   changeColor(){
-    this.setState({toggled: !this.state.toggled})
+    this.setState({checked_out: !this.state.checked_out})
   }
+
+  changeCheckoutTime(){
+    this.setState({checkOutTime: new Date()})
+  }
+
+  handleChange(event) {
+  this.setState({value: event.target.value});
+}
+
+handleSubmit(event) {
+  event.preventDefault();
+}
+  checkOut(){
+    this.changeColor();
+    this.changeCheckoutTime();
+  }
+
+
 
   // const styles = theme => ({
   //   card: {
@@ -24,33 +47,48 @@ class CaseComponent extends Component {
   // });
 
   render() {
-    let box_class = this.state.toggled ?   "greenBox" : "redBox";
-
-
-    var datetime = new Date();
+    const isCheckedOut = this.state.checked_out;
+    let box_class = this.state.checked_out ?  "redBox" : "greenBox" ;
+    const currtime = this.state.checkOutTime;
     var data = this.props.data;
     return (
       <div >
         <div style={{textAlgin:"center"}}>
         	<div className={box_class} >
         		<form style={{display:"flex"}}>
-        		<div style={{margin:"10px"}}>
-        			<p> ID {this.props.id} </p>
-        		</div>
-        		<div style = {{margin:"15px"}}>
-        			<label>
-        				Renter: <br />
-    					<input type="text" name="name" />
-  					</label>
-  				</div>
-  				<div style={{margin:"15px"}}>
-        			<label>
-        				In Use: <br />
-    					<input type="checkbox"  onClick={this.changeColor.bind(this)} name="name" />
-  					</label>
-  				</div>
+          		<div style={{margin:"10px"}}>
+          			<p> ID {this.props.id} </p>
+          		</div>
+
+              {isCheckedOut ?
+                (<div style = {{margin:"15px;width:20%"}}>{this.state.value}
+                  <button onClick={ this.checkOut.bind(this)}>Return</button> </div>)
+
+                :
+                (<div style = {{margin:"15px"}}>
+                <form onSubmit={this.handleSubmit}>
+                    <label>
+                      Name:
+                      <input type="text" value={this.state.value} onChange={this.handleChange} />
+                    </label>
+                      <input type="submit" value="Submit" onClick={ this.checkOut.bind(this)} />
+                </form>
+    				          </div>)
+                    }
+
+
           <div style={{margin:"10px"}}>
             <p> Charger Type: <b>{this.props.type} </b> </p>
+          </div>
+
+          <div style={{margin:"15px"}}>
+             <label >
+
+                {isCheckedOut ?
+                  (<div>Checked out: {currtime.toLocaleString()}</div>):
+                    (<div>Available</div>) }
+
+            </label>
           </div>
 
         		</form>
